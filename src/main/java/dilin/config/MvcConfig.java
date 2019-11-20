d65e00b69324167d3c68c6276ce24570b9618719
GulDilin <zhenyagurin@gmail.com>
2019-11-20 23:34:07 +0300

    Initial commit

---
+++ src/main/java/dilin/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package dilin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+        registry.addViewController("/hello.html").setViewName("hello");
+        registry.addViewController("/index.html").setViewName("index");
+    }
+
+
+}
